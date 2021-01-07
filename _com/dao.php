484bdf29c969f9345816bd732e37e7f0f19cf70a
Adrian Salgado <adri_salgadosanchez@hotmail.es>
2021-01-07 13:10:29 +0100

    categoriaEliminar dao metodo eliminar

--- _com/dao.php
+++ _com/dao.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "Varios.php";
+require_once "varios.php";
 
 class DAO
 {
@@ -28,20 +28,7 @@ class DAO
         return $pdo;
     }
 
-    private static function ejecutarConsulta(string $sql, array $parametros): array
-    {
-        if (!isset(self::$pdo)) self::$pdo = self::obtenerPdoConexionBd();
 
-        $select = self::$pdo->prepare($sql);
-        $select->execute($parametros);
-        $rs = $select->fetchAll();
-
-        return $rs;
-    }
-
-    // Devuelve:
-    //   - null: si ha habido un error
-    //   - 0, 1 u otro número positivo: OK (no errores) y estas son las filas afectadas.
     private static function ejecutarActualizacion(string $sql, array $parametros): ?int
     {
         if (!isset(self::$pdo)) self::$pdo = self::obtenerPdoConexionBd();
@@ -53,53 +40,16 @@ class DAO
         else return $actualizacion->rowCount();
     }
 
-    /* CATEGORÍA */
 
-    private static function categoriaCrearDesdeRs(array $fila): Categoria
+    public static function categoriaEliminar(int $id): ?int
     {
-        return new Categoria($fila["id"], $fila["nombre"]);
-    }
-
-    public static function categoriaObtenerPorId(int $id): ?Categoria
-    {
-        $rs = self::ejecutarConsulta(
-            "SELECT * FROM Categoria WHERE id=?",
+        $resultado = self::ejecutarActualizacion(
+            "DELETE FROM Categoria WHERE id=?",
             [$id]
         );
-        if ($rs) return self::crearCategoriaDesdeRs($rs[0]);
-        else return null;
-    }
 
-    public static function categoriaActualizar($id, $nombre)
-    {
-        self::ejecutarActualizacion(
-            "UPDATE Categoria SET nombre=? WHERE id=?",
-            [$nombre, $id]
-        );
-    }
-
-    public static function categoriaCrear(string $nombre)
-    {
-        self::ejecutarActualizacion(
-            "INSERT INTO Categoria (nombre) VALUES (?)",
-            [$nombre]
-        );
-    }
-
-    public static function categoriaObtenerTodas(): array
-    {
-        $datos = [];
-
-        $rs = self::ejecutarConsulta(
-            "SELECT * FROM Categoria ORDER BY nombre",
-            []
-        );
-
-        foreach ($rs as $fila) {
-            $categoria = self::categoriaCrearDesdeRs($fila);
-            array_push($datos, $categoria);
-        }
-
-        return $datos;
+        return $resultado;
     }
 }
+
+
--- categoriaEliminar.php
+++ categoriaEliminar.php
@@ -1,22 +1,46 @@
 <?php
-require_once "_com/_varios.php";
 
-$conexionBD = obtenerPdoConexionBD();
+require_once "_com/dao.php";
 
+
+// Se recoge el parámetro "id" de la request.
 $id = (int)$_REQUEST["id"];
 
-$sql = "DELETE FROM categoria WHERE id=?";
-
-
-$sentencia = $conexionBD->prepare($sql);
-
-$sqlConExito = $sentencia->execute([$id]);
-
-$correctoNormal = ($sqlConExito && $sentencia->rowCount() == 1);
-
-$noExistia = ($sqlConExito && $sentencia->rowCount() == 0);
-
-// INTERFAZ: $sqlConExito $correctoNormal $noExistia
+$resultado = dao::categoriaEliminar($id);
 ?>
 
+
+
+<html>
+
+<head>
+    <meta charset='UTF-8'>
+</head>
+
+
+
+<body>
+
+<?php if ($resultado == 1) { ?>
+
+    <h1>Eliminación completada</h1>
+    <p>Se ha eliminado correctamente la categoría.</p>
+
+<?php } else if ($resultado == 0) { ?>
+
+    <h1>Eliminación no realizada</h1>
+    <p>No existe la categoría que se pretende eliminar (quizá la eliminaron en paraleo o, ¿ha manipulado Vd. el parámetro id?).</p>
+
+<?php } else { ?>
+
+    <h1>Error en la eliminación</h1>
+    <p>No se ha podido eliminar la categoría.</p>
+
+<?php } ?>
+
+<a href='categoriaListado.php'>Volver al listado de categorías.</a>
+
+</body>
+
+</html>
 
