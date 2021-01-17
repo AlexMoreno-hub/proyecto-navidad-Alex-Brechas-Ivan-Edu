88573bd142bb1ee95cbf9993fea11a484ef1c718
Adrian Salgado <adri_salgadosanchez@hotmail.es>
2021-01-17 20:49:11 +0100

    paginaPrincipal

---
+++ paginaPrincipal.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title></title>
+</head>
+<body style="background-color: burlywood">
+
+<h1 style="text-align: center">Bienvenidos a "CREA TU EQUIPO IDEAL"</h1>
+<h3>Aqui podrás crear el equipo y los jugadores con las respectivas posiciones que quieras.</h3>
+
+<ul>
+    <li>Inicio</li>
+        <a href="categoriaListado.php">Posiciones</a>
+        <a href="equipoListado.php">Equipos</a>
+        <a href="jugadorLesionado.php">Jugadores</a>
+</ul>
+
+</body>
+</html>
+
